1d484f6027465d5b9160bef86a20494b201838a3
Ilia Makhonin <noizemkutane77@gmail.com>
2019-04-28 23:29:00 +0300

    Created functions for getting a path, added some paths

--- configuration/__path.js
+++ configuration/__path.js
@@ -1,14 +1,38 @@
 const path = require('path');
 
 
+// The basics paths to main folders of a project
 const srcBasePath = path.resolve(__dirname, '..', 'src');
 const publicBasePath = path.resolve(__dirname, '..', 'public');
+const distBasePath = path.resolve(__dirname, '..', 'dist');
+
+// The paths to main files of a React.js application
 const mainEntryPointPath = path.join(srcBasePath, 'index.js');
 const mainAppFilePath = path.join(srcBasePath, 'App.js');
-const appCataloguePath = path.join(srcBasePath, 'app');
 
-const distBasePath = path.resolve(__dirname, '..', 'dist');
+// The paths to main folders of a React.js application
+const appCataloguePath = path.join(srcBasePath, 'app');
+const srcImagesPath = path.join(appCataloguePath, 'assets', 'images');
+/**
+ * app
+ *   components
+ *   containers
+ *   styles
+ *   and etc
+ *   ...
+ */
+
+// The paths to dist main folders
 const distImagesPath = path.resolve(distBasePath, 'images');
+
+
+// ***************** Functions for getting path to some directory *****************
+
+const srcGetPath = (...pathFor) => path.join(srcBasePath, ...pathFor);
+const distGetPath = (...pathFor) => path.join(distBasePath, ...pathFor);
+const publicGetPath = (...pathFor) => path.join(publicBasePath, ...pathFor);
+
+// ********************************************************************************
 
 
 module.exports = {
@@ -17,6 +41,7 @@ module.exports = {
     mainEntryPointPath,
     mainAppFilePath,
     appCataloguePath,
+    srcImagesPath,
     distBasePath,
     distImagesPath
 };
