dbf0f1cee151830128cd08d1219b855346dcba3c
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-02 02:34:01 +0300

    Added new paths for the Project. In the future, I should clear this file of unnecessary variables

--- configuration/__path.js
+++ configuration/__path.js
@@ -6,40 +6,44 @@ const srcBasePath = path.resolve(__dirname, '..', 'src');
 const publicBasePath = path.resolve(__dirname, '..', 'public');
 const distBasePath = path.resolve(__dirname, '..', 'dist');
 
-const publicHTMLPath = path.join(publicBasePath, 'index.html');
-const publicFaviconPath = path.join(publicBasePath, 'favicon.ico');
-const publicManifestPath = path.join(publicBasePath, 'manifest.json');
+// The paths to public files: index.html, favicon.ico, manifest.json
+const publicHTMLPath = path.resolve(publicBasePath, 'index.html');
+const publicFaviconPath = path.resolve(publicBasePath, 'favicon.ico');
+const publicManifestPath = path.resolve(publicBasePath, 'manifest.json');
 
 // The paths to main files of a React.js application
-const mainEntryPointPath = path.join(srcBasePath, 'index.js');
-const mainAppFilePath = path.join(srcBasePath, 'App.js');
+const mainEntryPointPath = path.resolve(srcBasePath, 'index.js');
+const mainAppFilePath = path.resolve(srcBasePath, 'App.js');
 
 // The paths to main folders of a React.js application
-const appCataloguePath = path.join(srcBasePath, 'app');
+const appCataloguePath = path.resolve(srcBasePath, 'app');
 
 // And other: video, audio, etc
-const srcImagesPath = path.join(appCataloguePath, 'assets', 'images');
-const srcStylesPath = path.join(appCataloguePath, 'assets', 'styles');
-const srcCSSPath = path.join(srcStylesPath, 'css');
-const srcSCSSPath = path.join(srcStylesPath, 'scss');
-const srcLESSPath = path.join(srcStylesPath, 'less');
-const srcComponentsPath = path.join(appCataloguePath, 'components');
-const srcContainersPath = path.join(appCataloguePath, 'containers');
+const appAssetsPath = path.resolve(appCataloguePath, 'assets');
+const srcComponentsPath = path.resolve(appCataloguePath, 'components');
+const srcContainersPath = path.resolve(appCataloguePath, 'containers');
+
+const srcImagesPath = path.resolve(appAssetsPath, 'images');
+const srcStylesPath = path.resolve(appAssetsPath, 'styles');
+
+const srcCSSPath = path.resolve(srcStylesPath, 'css');
+const srcSCSSPath = path.resolve(srcStylesPath, 'scss');
+const srcLESSPath = path.resolve(srcStylesPath, 'less');
 
 // The paths for Redux components
-const storePath = path.join(appCataloguePath, 'store');
-const reducersPath = path.join(appCataloguePath, 'reducers');
-const actionsPath = path.join(appCataloguePath, 'actions');
+const storePath = path.resolve(appCataloguePath, 'store');
+const reducersPath = path.resolve(appCataloguePath, 'reducers');
+const actionsPath = path.resolve(appCataloguePath, 'actions');
 
 // The paths to dist main folders
-const distImagesPath = path.resolve(distBasePath, 'images');
+const distImagesPath = 'assets';
 
 
 // ***************** Functions for getting path to some directory *****************
 
-const srcGetPath = (...pathFor) => path.join(srcBasePath, ...pathFor);
-const distGetPath = (...pathFor) => path.join(distBasePath, ...pathFor);
-const publicGetPath = (...pathFor) => path.join(publicBasePath, ...pathFor);
+const srcGetPath = (...pathFor) => path.resolve(srcBasePath, ...pathFor);
+const distGetPath = (...pathFor) => path.resolve(distBasePath, ...pathFor);
+const publicGetPath = (...pathFor) => path.resolve(publicBasePath, ...pathFor);
 
 // ********************************************************************************
 
@@ -53,6 +57,7 @@ module.exports = {
     mainEntryPointPath,
     mainAppFilePath,
     appCataloguePath,
+    appAssetsPath,
     srcImagesPath,
     srcStylesPath,
     srcCSSPath,
