a3df3c86767dbb27876caee76ee5cdb116eb34bd
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-02 02:32:11 +0300

    ES-Lint config has been updated

--- configuration/alias.js
+++ configuration/alias.js
@@ -1,9 +1,17 @@
 const path = require('path');
-const _path_ = require('./__path');
+const _path_ = require('./configuration/__path');
 
 
 const aliasList = {
-    // Some path to modules and components
+    'components': _path_.srcComponentsPath,
+    'containers': _path_.srcContainersPath,
+    'assets': _path_.appAssetsPath,
+    'styles': '../src/app/assets/styles',
+    'images': _path_.srcImagesPath,
+    'store': _path_.storePath,
+    'reducers': _path_.reducersPath,
+    'actions': _path_.actionsPath
 };
+
 
 module.exports = aliasList;
