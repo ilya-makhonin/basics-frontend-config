2c20d2ddcf58413bead7c620f18a12f1a5dbafbd
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-02 02:37:45 +0300

    Testing the alias for the Project

--- configuration/alias.js
+++ configuration/alias.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const _path_ = require('./configuration/__path');
+const _path_ = require('./__path');
 
 
 const aliasList = {
     'components': _path_.srcComponentsPath,
     'containers': _path_.srcContainersPath,
     'assets': _path_.appAssetsPath,
-    'styles': '../src/app/assets/styles',
+    'styles': _path_.srcStylesPath,
     'images': _path_.srcImagesPath,
     'store': _path_.storePath,
     'reducers': _path_.reducersPath,
