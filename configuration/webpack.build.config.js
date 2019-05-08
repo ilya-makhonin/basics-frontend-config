ee90b5a85ef3160963cacef2adb02d3956e74de9
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-08 23:45:54 +0300

    The build config for WebPack. It's in development

--- configuration/webpack.build.config.js
+++ configuration/webpack.build.config.js
@@ -1,28 +1,52 @@
-const path = require('path');
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const HWPConfig = require('./settingsForModeType/HtmlWebpackPluginConfig');
 const babelConfig = require('./babel.config')();
+const cssLoader = require('./settingsForModeType/cssLoader')('production');
+const lessLoader = require('./settingsForModeType/lessLoader');
+const scssLoader = require('./settingsForModeType/scssLoader');
+const styleOutput = require('./settingsForModeType/stylesOutput');
+
+const { options, forPlugin } = styleOutput('production', 'plugin');
+const miniCssPlugin = { loader: MiniCssExtractPlugin.loader, options };
 
 const alias = require('./alias');
 const _path_ = require('./__path');
 
 
 const buildConfig = {
-    // optimization: {
-    //     minimizer: [
-    //         new TerserPlugin({
-    //             test: /\.js(\?.*)?$/i,
-    //             exclude: /(node_modules|bower_components)/,
-    //             include: /\/src/
-    //         })
-    //     ]
-    // },
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({
+                terserOptions: {
+                    parse: { ecma: 8 },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2
+                    },
+                    mangle: { safari10: true },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        ascii_only: true
+                    }
+                },
+                cache: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ],
+        splitChunks: { chunks: 'all', name: false },
+        runtimeChunk: true
+    },
     devtool: "hidden-source-map",
     entry: _path_.mainEntryPointPath,
     output: {
@@ -48,48 +72,19 @@ const buildConfig = {
             {
                 test: /\.css$/,
                 exclude: /(node_modules|bower_components)/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            // import: true,
-                            camelCase: true,
-                            sourceMap: true,
-                        },
-                    }
-                ],
+                use: [ miniCssPlugin, cssLoader ],
                 include: [ _path_.srcCSSPath ]
             },
             {
-                test: /\.scss$/,
+                test: /\.(sa|sc)ss$/,
                 exclude: /(node_modules|bower_components)/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            implementation: require('sass')
-                        }
-                    }
-                ],
+                use: [ miniCssPlugin, cssLoader, scssLoader('production') ],
                 include: [ _path_.srcSCSSPath ]
             },
             {
                 test: /\.less$/,
                 exclude: /(node_modules|bower_components)/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ],
+                use: [ miniCssPlugin, cssLoader, lessLoader ],
                 include: [ _path_.srcLESSPath ]
             },
             {
@@ -99,7 +94,7 @@ const buildConfig = {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
-                            outputPath: _path_.distImagesPath,
+                            outputPath: 'assets/media',
                             publicPath: _path_.srcImagesPath
                         }
                     },
@@ -109,6 +104,7 @@ const buildConfig = {
     },
     resolve: { alias },
     plugins: [
+        new MiniCssExtractPlugin(forPlugin),
         new HtmlWebpackPlugin(HWPConfig('production')),
         new InterpolateHtmlPlugin({ 'SOURCE_URL': 'assets' }),
         new ManifestPlugin({ fileName: 'assets-manifest.json' }),
