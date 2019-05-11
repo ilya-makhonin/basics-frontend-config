47f3318c16af0f761525eec494d840e35134cc64
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-11 23:51:49 +0300

    Added hot-container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
 
 
 class App extends Component {
@@ -11,4 +12,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default hot(App);
