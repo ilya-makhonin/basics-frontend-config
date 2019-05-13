fcd132b2a05f0dd17f8ecfd8c036ca61384f6d1d
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-13 23:56:37 +0300

    Updated paths to folders of the Project

--- configuration/alias.js
+++ configuration/alias.js
@@ -1,16 +1,15 @@
-const path = require('path');
 const _path_ = require('./__path');
 
 
 const aliasList = {
-    'components': _path_.srcComponentsPath,
-    'containers': _path_.srcContainersPath,
-    'assets': _path_.appAssetsPath,
+    'components': _path_.srcGetPath('app', 'components'),
+    'containers': _path_.srcGetPath('app', 'containers'),
+    'assets': _path_.srcGetPath('app','assets'),
     'styles': _path_.srcStylesPath,
     'images': _path_.srcImagesPath,
-    'store': _path_.storePath,
-    'reducers': _path_.reducersPath,
-    'actions': _path_.actionsPath
+    'store': _path_.srcGetPath('app','store'),
+    'reducer': _path_.srcGetPath('app','reducer'),
+    'actions': _path_.srcGetPath('app','actions')
 };
 
 
