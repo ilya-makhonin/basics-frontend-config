414d72ef39a7cf8fa9016cb5eba67bca77f8434a
Ilia Makhonin <noizemkutane77@gmail.com>
2019-05-13 23:54:37 +0300

    Deleted extra code

--- configuration/__path.js
+++ configuration/__path.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 
+const root = path.resolve(__dirname, '..');
+
 // The basics paths to main folders of a project
-const srcBasePath = path.resolve(__dirname, '..', 'src');
-const publicBasePath = path.resolve(__dirname, '..', 'public');
-const distBasePath = path.resolve(__dirname, '..', 'dist');
+const srcBasePath = path.resolve(root, 'src');
+const publicBasePath = path.resolve(root, 'public');
+const distBasePath = path.resolve(root, 'dist');
 
 // The paths to public files: index.html, favicon.ico, manifest.json
 const publicHTMLPath = path.resolve(publicBasePath, 'index.html');
@@ -15,25 +17,13 @@ const publicManifestPath = path.resolve(publicBasePath, 'manifest.json');
 const mainEntryPointPath = path.resolve(srcBasePath, 'index.js');
 const mainAppFilePath = path.resolve(srcBasePath, 'App.js');
 
-// The paths to main folders of a React.js application
-const appCataloguePath = path.resolve(srcBasePath, 'app');
-
 // And other: video, audio, etc
-const appAssetsPath = path.resolve(appCataloguePath, 'assets');
-const srcComponentsPath = path.resolve(appCataloguePath, 'components');
-const srcContainersPath = path.resolve(appCataloguePath, 'containers');
-
-const srcImagesPath = path.resolve(appAssetsPath, 'images');
-const srcStylesPath = path.resolve(appAssetsPath, 'styles');
+const srcImagesPath = path.resolve(srcBasePath, 'app', 'assets', 'images');
+const srcStylesPath = path.resolve(srcBasePath, 'app', 'assets', 'styles');
 
 const srcCSSPath = path.resolve(srcStylesPath, 'css');
 const srcSCSSPath = path.resolve(srcStylesPath, 'scss');
 const srcLESSPath = path.resolve(srcStylesPath, 'less');
-
-// The paths for Redux components
-const storePath = path.resolve(appCataloguePath, 'store');
-const reducersPath = path.resolve(appCataloguePath, 'reducers');
-const actionsPath = path.resolve(appCataloguePath, 'actions');
 
 
 // ***************** Functions for getting path to some directory *****************
@@ -46,6 +36,7 @@ const publicGetPath = (...pathFor) => path.resolve(publicBasePath, ...pathFor);
 
 
 module.exports = {
+    root,
     srcBasePath,
     publicBasePath,
     publicHTMLPath,
@@ -53,17 +44,12 @@ module.exports = {
     publicManifestPath,
     mainEntryPointPath,
     mainAppFilePath,
-    appCataloguePath,
-    appAssetsPath,
     srcImagesPath,
     srcStylesPath,
     srcCSSPath,
     srcSCSSPath,
     srcLESSPath,
-    srcComponentsPath,
-    srcContainersPath,
-    storePath,
-    reducersPath,
-    actionsPath,
-    distBasePath
+    distBasePath,
+    srcGetPath,
+    distGetPath
 };
