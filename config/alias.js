5ee0cf09fca674fa4a26cddc89318dacec55c7b1
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-10 23:40:37 +0300

    Alias module exportes a function now

--- config/alias.js
+++ config/alias.js
@@ -1,19 +1,24 @@
 const _path_ = require('./__path');
 
 
-const aliasList = {
-    'components': _path_.srcGetPath('app', 'components'),
-    'containers': _path_.srcGetPath('app', 'containers'),
-    'assets': _path_.srcGetPath('app','assets'),
-    'styles': _path_.srcGetPath('app', 'assets', 'styles'),
-    'images': _path_.srcGetPath('app', 'assets', 'images'),
-    'store': _path_.srcGetPath('app','store'),
-    'reducer': _path_.srcGetPath('app','reducer'),
-    'actions': _path_.srcGetPath('app','actions'),
-    'constants': _path_.srcGetPath('app', 'constants'),
-    'utils': _path_.srcGetPath('app', 'utils'),
-    'react-dom': '@hot-loader/react-dom'
+const alias = (isHot=false) => {
+    const baseAlias = {
+        'components': _path_.srcGetPath('app', 'components'),
+        'containers': _path_.srcGetPath('app', 'containers'),
+        'assets': _path_.srcGetPath('app','assets'),
+        'styles': _path_.srcGetPath('app', 'assets', 'styles'),
+        'images': _path_.srcGetPath('app', 'assets', 'images'),
+        'store': _path_.srcGetPath('app','store'),
+        'reducer': _path_.srcGetPath('app','reducer'),
+        'actions': _path_.srcGetPath('app','actions'),
+        'constants': _path_.srcGetPath('app', 'constants'),
+        'utils': _path_.srcGetPath('app', 'utils'),
+    }
+
+    isHot ? baseAlias['react-dom'] = '@hot-loader/react-dom' : undefined;
+
+    return baseAlias;
 };
 
 
-module.exports = aliasList;
+module.exports = alias;
