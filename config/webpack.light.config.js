023f4c60a1c1f526487f5323cdecad9773ad36d4
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-11 22:49:54 +0300

    Updated config of WebPack for development an application. Also the config used by dev server

--- config/webpack.light.config.js
+++ config/webpack.light.config.js
@@ -10,75 +10,81 @@ const HWPConfig = require('./settingsLoadersAndPlugins/HtmlWebpackPluginConfig')
 const cssLoader = require('./settingsLoadersAndPlugins/cssLoader')('development');
 const lessLoader = require('./settingsLoadersAndPlugins/lessLoader');
 const scssLoader = require('./settingsLoadersAndPlugins/scssLoader');
-const styleOutput = require('./settingsLoadersAndPlugins/stylesOutput')('development');
+const styleOutput = require('./settingsLoadersAndPlugins/stylesOutput');
 const alias = require('./alias');
 const _path_ = require('./__path');
 
 
-const webPackConfigure = {
-    devtool: 'source-map',
-    devServer: devServerConfig,
-    entry: {
-        app: [
-            'react-hot-loader/patch',
-            _path_.mainEntryPointPath,
+const webPackConfigure = (isHot=true) => {
+    const styleLoader = styleOutput('development', 'loader', isHot);
+    const entryCustome = isHot
+        ? { app: [ 'react-hot-loader/patch', _path_.mainEntryPointPath ] } 
+        : _path_.mainEntryPointPath;
+    const aliasCustome = alias(isHot);
+    const scssLoaderCustome = scssLoader('development');
+    const lessLoaderCustome = lessLoader('development');
+    const configForHWP = HWPConfig('development');
+
+    return{
+        devtool: 'source-map',
+        devServer: devServerConfig,
+        entry: entryCustome,
+        output: {
+            path: _path_.distBasePath,
+            filename: '[name].build.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    enforce: 'pre',
+                    use: 'eslint-loader',
+                    include: [ _path_.srcBasePath ]
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: 'babel-loader',
+                    include: [ _path_.srcBasePath ]
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: [ styleLoader, cssLoader ],
+                    include: [ _path_.srcBasePath ]
+                },
+                {
+                    test: /\.(sa|sc)ss$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: [ styleLoader , cssLoader, scssLoaderCustome ],
+                    include: [ _path_.srcBasePath ]
+                },
+                {
+                    test: /\.less$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: [ styleLoader, cssLoader, lessLoaderCustome ],
+                    include: [ _path_.srcBasePath ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+                    use: [ { loader: 'url-loader', options: { limit: 10000 } } ]
+                }
+            ]
+        },
+        resolve: { alias: aliasCustome },
+        plugins: [
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin(configForHWP),
+            new InterpolateHtmlPlugin({ 'SOURCE_URL': '.' }),
+            new CopyWebpackPlugin([
+                { from: _path_.publicGetPath('favicon.ico'), to: _path_.distBasePath },
+                { from: _path_.publicGetPath('manifest.json'), to: _path_.distBasePath },
+            ])
         ]
-    },
-    output: {
-        path: _path_.distBasePath,
-        filename: '[name].build.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                enforce: 'pre',
-                use: 'eslint-loader',
-                include: [ _path_.srcBasePath ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: 'babel-loader',
-                include: [ _path_.srcBasePath ]
-            },
-            {
-                test: /\.css$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [ styleOutput, cssLoader ],
-                include: [ _path_.srcBasePath ]
-            },
-            {
-                test: /\.(sa|sc)ss$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [ styleOutput, cssLoader, scssLoader('development') ],
-                include: [ _path_.srcBasePath ]
-            },
-            {
-                test: /\.less$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [ styleOutput, cssLoader, lessLoader('development') ],
-                include: [ _path_.srcBasePath ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-                use: [ { loader: 'url-loader', options: { limit: 10000 } } ]
-            }
-        ]
-    },
-    resolve: { alias },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin( HWPConfig('development') ),
-        new InterpolateHtmlPlugin({ 'SOURCE_URL': '.' }),
-        new CopyWebpackPlugin([
-            { from: _path_.publicGetPath('favicon.ico'), to: _path_.distBasePath },
-            { from: _path_.publicGetPath('manifest.json'), to: _path_.distBasePath },
-        ])
-    ]
+    }
 };
 
 module.exports = webPackConfigure;
