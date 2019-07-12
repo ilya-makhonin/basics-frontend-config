7036f9796cfa45656f58c33752272725b386a37e
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-12 23:48:14 +0300

    First version of config for postCSS

---
+++ config/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        'postcss-import': {},
+        'postcss-global-import': {},
+        'postcss-nested': {},
+        'autoprefixer': {},
+        'postcss-flexbugs-fixes': {}
+    },
+};
